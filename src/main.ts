d19621c5d44e12fea23c69c8ec88ef35e934bd85
Radmacher <git@radmacher.me>
2021-10-17 21:58:15 +0200

    The website will now verify email addresses

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import {createApp} from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+// Windi CSS
+import 'virtual:windi.css'
+
+createApp(App).mount('#app');
