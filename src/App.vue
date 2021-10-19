838f91b7dce52fa98123d0e714b004d9b9ce10a5
Radmacher <git@radmacher.me>
2021-10-19 20:31:30 +0200

    Debugging problems

--- src/App.vue
+++ src/App.vue
@@ -10,14 +10,20 @@ appwrite.setEndpoint(endpoint).setProject(project);
 
 const parameters = new URLSearchParams(window.location.search);
 
-let successful = false;
+let error = '';
+let success = false;
 
 if (parameters.has('userId') && parameters.has('secret')) {
   const userId = parameters.get('userId') as string;
   const secret = parameters.get('secret') as string;
 
-  appwrite.account.updateVerification(userId, secret);
-  successful = true;
+  appwrite.account.updateVerification(userId, secret).then((res) => {
+    console.log(res)
+  }, (err) => {
+    error = err;
+    console.log(err);
+  });
+  success = true;
 }
 </script>
 
